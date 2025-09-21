cbdd1631231015b871abe38e11d61e3d18c390cf
contemplationnn <yusup.ahtaev.095@bk.ru>
2025-09-21 22:05:22 +0300

    Update calculator.py

--- portfolio-simple/python-calculator/calculator.py
+++ portfolio-simple/python-calculator/calculator.py
@@ -1,1 +1,28 @@
+print("=== Калькулятор на Python ===")
+print("Доступные операции: +, -, *, /")
+print("Введите 'exit' чтобы выйти")
+
+result = float(input("Введите первое число: "))
+
+while True:
+    operator = input("Введите операцию (+, -, *, /): ")
+    if operator.lower() == "exit":
+        print("Выход из программы...")
+        break
+
+    number = float(input("Введите число: "))
+
+    if operator == "+":
+        result = result + number
+    elif operator == "-":
+        result = result - number
+    elif operator == "*":
+        result = result * number
+    elif operator == "/":
+        result = result / number
+    else:
+        print("Ошибка: неизвестная операция!")
+        continue
+
+    print("Текущий результат:", result)
 
